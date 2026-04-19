a313dfa356ed3abe7f84d040816f4e573d1e3c30
lomantochris-byte <lomanto.chris@gmail.com>
2026-04-19 09:40:00 -0700

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <main style={{ padding: "60px" }}>
+      <h1>Contact</h1>
+
+      <p style={{ marginTop: "20px" }}>
+        Email: info@dnfkids.com
+      </p>
+    </main>
+  );
+}
