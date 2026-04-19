d213d3b3583e2806fc02c6293c7cc715a0cff991
lomantochris-byte <lomanto.chris@gmail.com>
2026-04-19 13:08:44 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,13 +4,13 @@ export default function About() {
       <h1>About DNF KIDS</h1>
 
       <p style={{ marginTop: "20px", maxWidth: "700px" }}>
-        DNF KIDS is a 501(c)(3) registered nonprofit organization dedicated to protecting children
+        DNF KIDS is a California registered 501(c)(3) nonprofit organization dedicated to protecting children
         through advocacy, support, and stopping child predators.
       </p>
 
       <p style={{ marginTop: "20px", maxWidth: "700px" }}>
-        We do not conduct criminal investigations. We DO support law enforcment and prosecutors with Tools, Resources and Information necessary to capture chlid predators.
-        We DO provide advocacy, assistance and justice for the Child Victims.
+        We provide advocacy, assistance and justice for Child Sex Abuse Victims. We accomplish this through our extensive network of specialized care providers, our research into the very best therapeutic practices and through our support for law enforcment and prosecutors by providing the Tools, Resources and Information necessary to capture chlid predators.
+        
       </p>
     </main>
   );
